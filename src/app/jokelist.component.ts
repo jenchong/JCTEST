98518ad274c927f98c6b24121d19e86824b3c61c
jenchong <jenchong@primalux.com.sg>
2018-04-20 11:07:47 +0800

    JC add Hahaha

--- src/app/jokelist.component.ts
+++ src/app/jokelist.component.ts
@@ -13,7 +13,7 @@ export class JokeListComponent {
     
     constructor() {
         this.jokes = [
-            new Joke ('What did the cheese say when looked into the mirror?', `Halloumi (Hello Me X0 XO)`), 
+            new Joke ('What did the cheese say when looked into the mirror Hahahaha?', `Halloumi (Hello Me X0 XO)`), 
             new Joke ('What kind of cheese do you use to disguise a small horse?', 'Mask-a-pony (Mascarpone)'),
             new Joke ('A kid threw a lump of xxxcheddar at me','I thought \'That\'s not very mature but is really hilarious .\''),
     ];
